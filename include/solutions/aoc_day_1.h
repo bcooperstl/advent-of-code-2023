ff5e9ed73c141c4a251856fc02e44326acf1f6bd
Brian Cooper <coop999@gmail.com>
2023-12-01 15:56:11 +0000

    Added part 2 soltuion - passes test case

--- include/solutions/aoc_day_1.h
+++ include/solutions/aoc_day_1.h
@@ -8,11 +8,12 @@ class AocDay1 : public AocDay
     private:
         vector<string> read_input(string filename);
         long get_calibration_value(string line);
+        long get_calibration_value_with_words(string line);
     public:
         AocDay1();
         ~AocDay1();
         string part1(string filename, vector<string> extra_args);
-        //string part2(string filename, vector<string> extra_args);
+        string part2(string filename, vector<string> extra_args);
 };
 
 
