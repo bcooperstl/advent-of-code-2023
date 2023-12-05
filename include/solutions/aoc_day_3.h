3610b183da1b2537338a2ce11781e836ce5c8373
Brian Cooper <coop999@gmail.com>
2023-12-05 03:02:46 +0000

    Added find_part_numbers function; works for test case

--- include/solutions/aoc_day_3.h
+++ include/solutions/aoc_day_3.h
@@ -16,12 +16,13 @@ namespace Day3
             char m_schematic[DAY_3_MAX_LENGTH+2][DAY_3_MAX_LENGTH+2+1];
             int m_num_rows; // actual number of rows with input data
             int m_num_cols; // actual number of cols with input data
+            bool has_neighbor_symbol(int row, int start_col, int end_col);
         public:
             Schematic();
             ~Schematic();
             void load_schematic(vector<string> input_data);
             void display_schematic();
-            //vector<int> find_part_numbers();
+            vector<int> find_part_numbers();
     };    
 }
 
--- src/solutions/aoc_day_3.cpp
+++ src/solutions/aoc_day_3.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <cstring>
+#include <cctype>
 
 #include "aoc_day_3.h"
 #include "file_utils.h"
@@ -59,6 +60,98 @@ namespace Day3
         }
         cout << endl;
     }
+    
+    // This function assumes the row and cols are in range and border can be examined
+    // This function also assumes that there aren't digits to the left of start and right of end
+    bool Schematic::has_neighbor_symbol(int row, int start_col, int end_col)
+    {
+#ifdef DEBUG_DAY_3
+        cout << " Checking for neighboring symbols for row " << row << " cols " << start_col << "-" << end_col << endl;
+#endif
+        // check left for start col
+        if (m_schematic[row][start_col-1] != NON_SYMBOL)
+        {
+#ifdef DEBUG_DAY_3
+            cout << "  Symbol " << m_schematic[row][start_col-1] << " found at row " << row << " column " << start_col - 1 << endl;
+#endif
+            return true;
+        }
+        // check right for end col
+        if (m_schematic[row][end_col+1] != NON_SYMBOL)
+        {
+#ifdef DEBUG_DAY_3
+            cout << "  Symbol " << m_schematic[row][end_col+1] << " found at row " << row << " column " << end_col + 1 << endl;
+#endif
+            return true;
+        }
+        // check above and below rows from start_col-1 to end_col+1 to count for diagonals
+        for (int col=(start_col-1); col<=(end_col+1); col++)
+        {
+            // above row
+            if (m_schematic[row-1][col] != NON_SYMBOL)
+            {
+#ifdef DEBUG_DAY_3
+                cout << "  Symbol " << m_schematic[row-1][col] << " found at row " << row-1 << " column " << col << endl;
+#endif
+                return true;
+            }
+            // below row
+            if (m_schematic[row+1][col] != NON_SYMBOL)
+            {
+#ifdef DEBUG_DAY_3
+                cout << "  Symbol " << m_schematic[row+1][col] << " found at row " << row+1 << " column " << col << endl;
+#endif
+                return true;
+            }
+        }
+#ifdef DEBUG_DAY_3
+        cout << "  No symbol found" << endl;
+#endif
+        return false;
+    }
+    
+    vector<int> Schematic::find_part_numbers()
+    {
+        vector<int> part_numbers;
+        
+        int part_number, match_start_col, match_end_col;
+        for (int row=1; row<=m_num_rows; row++)
+        {
+            for (int col=1; col<=m_num_cols; col++)
+            {
+                if (isdigit(m_schematic[row][col])) // found the leading digit
+                {
+                    part_number = 0;
+                    match_start_col = col;
+                    while (isdigit(m_schematic[row][col]))
+                    {
+                        part_number = (part_number * 10) + m_schematic[row][col]-'0';
+                        match_end_col = col;
+                        col++;
+                    }
+                    // at this point, m_schematic[row][col] is not a digit. need to check if the value we found has a symbol neighbor or not
+#ifdef DEBUG_DAY_3
+                    cout << "Found " << part_number << " at row " << row << " columns " << match_start_col << "-" << match_end_col << ". Checking if neighbor has symbol" << endl;
+#endif
+                    if (has_neighbor_symbol(row, match_start_col, match_end_col))
+                    {
+#ifdef DEBUG_DAY_3
+                        cout << " Matching neighbor symbol found for " << part_number << endl;
+#endif
+                        part_numbers.push_back(part_number);
+                    }
+                    else
+                    {
+#ifdef DEBUG_DAY_3
+                        cout << " No matching neighbor symbol found for " << part_number << endl;
+#endif
+                    }
+                }
+            }
+        }
+        
+        return part_numbers;
+    }
 }
 
 AocDay3::AocDay3():AocDay(3)
@@ -86,8 +179,13 @@ string AocDay3::part1(string filename, vector<string> extra_args)
     Schematic schematic;
     schematic.load_schematic(data);
     
-    int sum = 0;
+    vector<int> part_numbers = schematic.find_part_numbers();
     
+    int sum = 0;
+    for (int i=0; i<part_numbers.size(); i++)
+    {
+        sum+=part_numbers[i];
+    }
     
     ostringstream out;
     out << sum;
