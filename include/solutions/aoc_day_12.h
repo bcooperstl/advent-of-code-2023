6d6fff5a79b0566de5f8eef8bdabaa487cf234e0
Brian Cooper <coop999@gmail.com>
2024-02-23 04:39:51 +0000

    Added code to find starting positions for each group

--- include/solutions/aoc_day_12.h
+++ include/solutions/aoc_day_12.h
@@ -7,9 +7,17 @@
 
 #define DAY12_MAX_LENGTH 50
 #define DAY12_MAX_GROUPS 10
+#define DAY12_MAX_GROUP_LENGTH 16
 
 namespace Day12
 {
+    struct Positions
+    {
+        bool processed[DAY12_MAX_GROUP_LENGTH+1];
+        int start_positions[DAY12_MAX_GROUP_LENGTH+1][DAY12_MAX_LENGTH];
+        int num_good_positions[DAY12_MAX_GROUP_LENGTH+1];
+    };
+    
     class Group
     {
         private:
@@ -19,10 +27,13 @@ namespace Day12
             int m_num_groups;
             bool can_be_operational(int pos);
             bool can_be_damaged(int pos);
+            void find_starting_positions(Positions * positions);
+            void display();
         public:
             Group();
             ~Group();
             void load_group(vector<string> data);
+            int get_num_arrangements();
     };
     
     class Groups
@@ -33,6 +44,7 @@ namespace Day12
             Groups();
             ~Groups();
             void load_groups(vector<vector<string>> data);
+            int get_num_arrangements();
     };
 }
 
--- src/solutions/aoc_day_12.cpp
+++ src/solutions/aoc_day_12.cpp
@@ -37,14 +37,18 @@ namespace Day12
         m_num_groups = data.size()-1;
         
 #ifdef DEBUG_DAY_12
+        display();
+#endif
+    }
+    
+    void Group::display()
+    {
         cout << "Group " << m_conditions << " has groups of:";
         for (int i=0; i<m_num_groups; i++)
         {
             cout << ' ' << m_groups[i];
         }
         cout << endl;
-#endif
-    
     }
     
     bool Group::can_be_operational(int pos)
@@ -56,6 +60,78 @@ namespace Day12
     {
         return (m_conditions[pos] == DAMAGED || m_conditions[pos] == UNKNOWN);
     }
+    
+    void Group::find_starting_positions(Positions * positions)
+    {
+#ifdef DEBUG_DAY_12
+        cout << "Finding starting positions for ";
+        display();
+#endif
+        for (int i=1; i<=DAY12_MAX_GROUP_LENGTH; i++)
+        {
+            positions->processed[i] = false;
+            positions->num_good_positions[i] = 0;
+        }
+        
+        for (int group=0; group<m_num_groups; group++)
+        {
+            int length = m_groups[group];
+            if (positions->processed[length] == true)
+            {
+#ifdef DEBUG_DAY_12
+                cout << " Skipping group " << group << " because " << length << " is already processed" << endl;
+#endif
+                continue;
+            }
+#ifdef DEBUG_DAY_12
+            cout << " Processing group " << group << " with value " << length << endl;
+#endif
+            for (int start=0; start<=m_condition_length-length; start++)
+            {
+#ifdef DEBUG_DAY_12
+                cout << "  Checking at start " << start << " ";
+#endif
+                bool good = true;
+                if (start > 0 && m_conditions[start-1] == DAMAGED)
+                {
+                    good = false;
+#ifdef DEBUG_DAY_12
+                    cout << "Does not work because of damaged at " << start - 1 << endl;
+#endif
+                    continue;
+                }
+                for (int pos=start; pos<start+length; pos++)
+                {
+                    if (!can_be_damaged(pos))
+                    {
+                        good = false;
+#ifdef DEBUG_DAY_12
+                        cout << "Does not work because of operational at " << pos << endl;
+#endif
+                        break;
+                    }
+                }
+                if (good)
+                {
+#ifdef DEBUG_DAY_12
+                    cout << "Works as a starting position" << endl;
+#endif
+                    positions->start_positions[length][positions->num_good_positions[length]] = start;
+                    positions->num_good_positions[length]++;
+                }
+            }
+            positions->processed[length] = true;
+        }
+    }
+    
+    int Group::get_num_arrangements()
+    {
+        Positions positions;
+        find_starting_positions(&positions);
+        
+        return 0;
+    }
+    
 
     Groups::Groups()
     {
@@ -73,6 +149,16 @@ namespace Day12
             group.load_group(data[i]);
             m_groups.push_back(group);
         }
+    }
+    
+    int Groups::get_num_arrangements()
+    {
+        int num_arrangements = 0;
+        for (int i=0; i<m_groups.size(); i++)
+        {
+            num_arrangements+=m_groups[i].get_num_arrangements();
+        }
+        return num_arrangements;
     }
 }
 
@@ -108,6 +194,6 @@ string AocDay12::part1(string filename, vector<string> extra_args)
     groups.load_groups(data);
     
     ostringstream out;
-    out << 0;
+    out << groups.get_num_arrangements();
     return out.str();
 }
